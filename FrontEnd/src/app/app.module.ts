c81c78d94e33883af36dce9ba89c89e8b1efe596
acer <https://github.com/SifatSikder/A2F/tree/master/Frontend>
2023-09-26 15:45:58 +0700

    added frontend

---
+++ FrontEnd/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { GridComponent } from './grid/grid.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    GridComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
